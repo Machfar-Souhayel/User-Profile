6d58038caaf995bb81f6cf9bf89ffbb1cb1340f1
Machfar-Souhayel <souhayel.isetch@gmail.com>
2019-10-24 22:55:56 +0100

    Footer still in progress

--- js/main.js
+++
@@ -1,9 +0,0 @@
-link = document.getElementById("already-have-acc");
-userInput = document.getElementsByClassName("login-submit")[0];
-
-link.addEventListener('click', function(){
-	window.setTimeout(function()
-	{
-		userInput.focus();
-	},0);
-});
